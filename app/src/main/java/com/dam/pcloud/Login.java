bc9e91148b9754a74b0f5f67dc04cb84a43ac5d3
Nelnitorian <jujoroal@gmail.com>
2023-05-18 21:49:55 +0200

    Implementados login y register

--- app/src/main/java/com/dam/pcloud/Login.java
+++ app/src/main/java/com/dam/pcloud/Login.java
@@ -6,16 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.toolbox.Volley;
+import com.dam.pcloud.rest.Error;
+import com.dam.pcloud.rest.HandlerCallBack;
+import com.dam.pcloud.rest.IPcloudRestHandler;
+import com.dam.pcloud.rest.PcloudRestHandler;
 
 public class Login extends AppCompatActivity {
 
     private EditText email;
     private EditText contrasenia;
 
+    private IPcloudRestHandler handler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        this.handler = new PcloudRestHandler(Volley.newRequestQueue(this));
     }
 
     public void clicEnRecuperarContra(View view) {
@@ -29,16 +40,20 @@ public class Login extends AppCompatActivity {
         Log.d("Inicio_sesion", "Email: " +email.getText()+ ". Contraseña: " +contrasenia.getText());
 
         //LLAMAR AL MÉTODO DE LOGIN
+        handler.login(email.getText().toString(), contrasenia.getText().toString(), new HandlerCallBack() {
+            @Override
+            public void onSuccess(Object obj) {
+                // Exito
+                Intent intent = new Intent(getApplicationContext(), Inicio.class);
+                startActivity(intent);
+            }
 
-        //Aquí comprobar que el login ha ido bien.
-            // En caso correcto nos vamos a inicio.
-        //if .........
-        Intent intent = new Intent(this, Inicio.class);
-        startActivity(intent);
-
-        //else ........
-            // En caso incorrecto permanecemos aquí y avisamos de error con un Toast
-
+            @Override
+            public void onError(Error error) {
+                // Error
+                Toast.makeText(getApplicationContext(), "Error "+error.getCode()+" al iniciar sesión: "+error.getDescription(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     public void clicEnRegistrarse(View view) {
         Intent intent = new Intent(this, Register.class);
--- app/src/main/java/com/dam/pcloud/Register.java
+++ app/src/main/java/com/dam/pcloud/Register.java
@@ -6,14 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.toolbox.Volley;
+import com.dam.pcloud.rest.Error;
+import com.dam.pcloud.rest.HandlerCallBack;
+import com.dam.pcloud.rest.IPcloudRestHandler;
+import com.dam.pcloud.rest.PcloudRestHandler;
 
 public class Register extends AppCompatActivity {
     private EditText email;
     private EditText contrasenia;
+    private IPcloudRestHandler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        this.handler = new PcloudRestHandler(Volley.newRequestQueue(this));
     }
 
 
@@ -27,16 +37,22 @@ public class Register extends AppCompatActivity {
         contrasenia= (EditText)findViewById(R.id.contrasenia);
         Log.d("Registro", "Email: " +email.getText()+ ". Contraseña: " +contrasenia.getText());
 
-        //LLAMAR AL MÉTODO DE REGISTER
+        //LLAMAR AL MÉTODO DE LOGIN
+        handler.register(email.getText().toString(), contrasenia.getText().toString(), new HandlerCallBack() {
+            @Override
+            public void onSuccess(Object obj) {
+                // Exito
+                Toast.makeText(getApplicationContext(), "Exito", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
 
-        //Aquí comprobar que el registro ha ido bien.
-            // En caso correcto nos vamos a inicio.
-            //if .........
-        Intent intent = new Intent(this, Inicio.class);
-        startActivity(intent);
-
-        //else ........
-        // En caso incorrecto permanecemos aquí y avisamos de error con un Toast
+            @Override
+            public void onError(Error error) {
+                // Error
+                Toast.makeText(getApplicationContext(), "Error "+error.getCode()+" al registrarse: "+error.getDescription(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
