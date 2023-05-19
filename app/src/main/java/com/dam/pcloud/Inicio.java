581976a98e481850c950362f49c2e5ba93b2936b
bellozjur <bellozjur@alum.us.es>
2023-05-19 14:59:06 +0200

    buscador y elementos list view implementados

--- app/src/main/java/com/dam/pcloud/Inicio.java
+++ app/src/main/java/com/dam/pcloud/Inicio.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -23,8 +25,10 @@ public class Inicio extends AppCompatActivity {
     private Spinner sPoints;
     private ListView lista;
     private SearchView buscar;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> items;
+    private int fotoItem;
+    private Adaptador adaptador;
+    private ArrayList<ListItem> items;
+    private boolean isFirstItemSelection = true; // Variable para controlar la primera selección del spinner
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,38 +37,48 @@ public class Inicio extends AppCompatActivity {
         lista = (ListView) findViewById(R.id.lista_archivos);
         buscar = (SearchView) findViewById(R.id.buscar);
 
+        //Para poder hacer referencia a un elemento que no está en Inicio.xml
+        View convertView = LayoutInflater.from(this).inflate(R.layout.formato_item, null);
+        sPoints = (Spinner) convertView.findViewById(R.id.despl_points);
+
+        //Dependiendo del formato del archivo ponemos una imagen personalizada
+        fotoItem= R.drawable.file;
 
         // ArrayList para almacenar los elementos de la lista
-        items = new ArrayList<>();
+        items = new ArrayList<ListItem>();
         for (int i = 0; i < lista.getCount(); i++) {
-            items.add(lista.getItemAtPosition(i).toString());
+            items.add(new ListItem(fotoItem,lista.getItemAtPosition(i).toString(),R.drawable.points));
         }
 
         //Aplicamos un formato personalizado a cada elemento de la lista
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        lista.setAdapter(adapter);
+        adaptador = new Adaptador(this, items);
+        lista.setAdapter(adaptador);
 
         //Filtro del buscador
         buscar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Inicio.this.adapter.getFilter().filter(query);
+                Inicio.this.adaptador.getFilter().filter(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                Inicio.this.adapter.getFilter().filter(newText);
+                Inicio.this.adaptador.getFilter().filter(newText);
                 return false;
             }
         });
 
 
-
-        //Aquí gestionamos lo que debe hacer el código según la opción seleccionada en el desplegable lanzado tras pulsar +
+        //Desplegable botón +
         sPlus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (isFirstItemSelection) {
+                    isFirstItemSelection = false;
+                    return; // Evita que se ejecute automáticamente en la primera selección
+                }
+
                 // Obtener el elemento seleccionado en el Spinner
                 String selectedItem = (String) parent.getItemAtPosition(position);
 
@@ -92,4 +106,5 @@ public class Inicio extends AppCompatActivity {
     public void clicEnPlusCircle(View view) {
         sPlus.performClick();
     }
+
 }
